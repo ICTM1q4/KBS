ea6eb9fbf2dffd5aa91346660f4582a309af50a9
Mirmidon <you@example.com>
2020-11-11 17:45:26 +0100

    slideshow index.html

--- Yorben/index.php
+++ Yorben/index.php
@@ -4,6 +4,37 @@
     <meta charset="UTF-8">
     <title>Title</title>
     <link rel='stylesheet' href='style.css'>
+    
+    <script>
+        var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
+    </script>
 </head>
 <header>
     <div id="totaal">
@@ -23,8 +54,6 @@
             
 
         </div>
-        
-        <div class="background">
         <input id="search" type="text" placeholder="search" style="right: ">
             <?php
             
@@ -37,18 +66,52 @@
                 print("<a id='login' href='Account.php' class='button' style='color: white;'>Account</a>");
             }
             ?>
-        </div>
+        
 
     </div>
 
 </header>
-<body>
+<body >
+    <div id="index">
+        <h1 > Goede Middag</h1>
 
 
+            <!-- Slideshow container -->
+<div class="slideshow-container">
+
+<!-- Full-width images with number and caption text -->
+<div class="mySlides fade">
+  <div class="numbertext">1 / 3</div>
+  <img src="img_lights_wide.jpg" style="width:100%;  position: absolute;">
+  <div class="text">Caption Text</div>
+</div>
+
+<div class="mySlides fade">
+  <div class="numbertext">2 / 3</div>
+  <img src="img_nature_wide.jpg" style="width:100%; position: absolute;">
+  <div class="text">Caption Two</div>
+</div>
+
+<div class="mySlides fade">
+  <div class="numbertext">3 / 3</div>
+  <img src="img_snow_wide.jpg" style="width:100%;  position: absolute;">
+  <div class="text">Caption Three</div>
+</div>
+
+<!-- Next and previous buttons -->
+<a class="prev" onclick="plusSlides(-1)">&#10094;</a>
+<a class="next" onclick="plusSlides(1)">&#10095;</a>
+</div>
+<br>
+
+<!-- The dots/circles -->
+<div style="text-align:center">
+<span class="dot" onclick="currentSlide(1)"></span>
+<span class="dot" onclick="currentSlide(2)"></span>
+<span class="dot" onclick="currentSlide(3)"></span>
+</div>
 
 
-
-
-
+    <div>
 </body>
 </html>
