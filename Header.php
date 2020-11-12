40526e2a075d9ec58e8ebd3ccdd7250511db3544
Mirmidion <yorben1192@gmail.com>
2020-11-12 22:18:16 +0100

    login button aangepast

--- Header.php
+++ Header.php
@@ -76,10 +76,10 @@ include "connect.php";
 
             $login = false;
             if ($login == false){
-                print("<a id='login' href='Login.php' class='button' style='color: white;'>login</a>");
+                print("<a id='login' href='Login.php' id='categories' class='button' style='color: white; font-family: Calibri; font-size: 150%;'>login</a>");
             }
             else {
-                print("<a id='login' href='Account.php' class='button' style='color: white;'>Account</a>");
+                print("<a id='login' href='Account.php' id='categories' class='button' style='color: white;'>Account</a>");
             }
             ?>
         </div>
