cc0496a1df10235176ce5de1c1f7431dc062f9a2
Mirmidon <you@example.com>
2020-11-12 13:08:29 +0100

    header include

--- browse.php
+++ browse.php
@@ -6,41 +6,9 @@
     <link rel='stylesheet' href='style.css'>
 </head>
 <header>
-    <div id="totaal">
-        <div id="titel">
-            <picture id="picture"><img src="wauw%20(1).png"></picture>
-            <h3> NerdyGadgets </h3>
-        </div>
-        <div id="categorie">
-            <a href="index.php" class="button">Home</a>
-            <a href="browse.php?category_id=" class="button">Novelty Items</a>
-            <a href="browse.php?category_id=" class="button">Clothing</a>
-            <a href="browse.php?category_id=" class="button">T-Shirts</a>
-            <a href="browse.php?category_id=" class="button">Computing Novelties</a>
-            <a href="browse.php?category_id=" class="button">USB Novelties</a>
-            <a href="browse.php?category_id=" class="button">Toys</a>
-            <a href="categories.php" class="button">Alle categorieen</a>
-            
-
-        </div>
-        
-        <div class="background">
-        <input id="search" type="text" placeholder="search" style="right: ">
-            <?php
-            
-
-            $login = false;
-            if ($login == false){
-                print("<a id='login' href='Login.php' class='button' style='color: white;'>login</a>");
-            }
-            else {
-                print("<a id='login' href='Account.php' class='button' style='color: white;'>Account</a>");
-            }
-            ?>
-        </div>
-
-    </div>
-
+<?php
+include __DIR__ . "/Header.php";
+?>
 </header>
 
 
--- index.php
+++ index.php
@@ -37,39 +37,9 @@ function showSlides(n) {
     </script>
 </head>
 <header>
-    <div id="totaal">
-        <div id="titel">
-            <picture id="picture"><img src="wauw%20(1).png"></picture>
-            <h3> NerdyGadgets </h3>
-        </div>
-        <div id="categorie">
-            <a href="index.php" class="button">Home</a>
-            <a href="browse.php?category_id=1" class="button">Novelty Items</a>
-            <a href="browse.php?category_id=2" class="button">Clothing</a>
-            <a href="browse.php?category_id=4" class="button">T-Shirts</a>
-            <a href="browse.php?category_id=6" class="button">Computing Novelties</a>
-            <a href="browse.php?category_id=7" class="button">USB Novelties</a>
-            <a href="browse.php?category_id=9" class="button">Toys</a>
-            <a href="categories.php" class="button">Alle categorieen</a>
-            
-
-        </div>
-        <input id="search" type="text" placeholder="search" style="right: ">
-            <?php
-            
-
-            $login = false;
-            if ($login == false){
-                print("<a id='login' href='Login.php' class='button' style='color: white;'>login</a>");
-            }
-            else {
-                print("<a id='login' href='Account.php' class='button' style='color: white;'>Account</a>");
-            }
-            ?>
-        
-
-    </div>
-
+<?php
+include __DIR__ . "/Header.php";
+?>
 </header>
 <body >
     <div id="index">
