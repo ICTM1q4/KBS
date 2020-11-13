92de74a4a6907db078af5600527491bc1685dd1b
Mirmidon <you@example.com>
2020-11-13 11:38:52 +0100

    .

--- index.php
+++ index.php
@@ -52,7 +52,10 @@ include __DIR__ . "/Header.php";
 <!-- Full-width images with number and caption text -->
 <div class="mySlides fade">
   <div class="numbertext">1 / 3</div>
-  <img src="img_lights_wide.jpg" style="width:100%;  position: absolute;">
+   <div id="parent">
+
+   
+  </div>
   <div class="text">Slide 1/3</div>
 </div>
 
@@ -80,6 +83,100 @@ include __DIR__ . "/Header.php";
 <span class="dot" onclick="currentSlide(2)"></span>
 <span class="dot" onclick="currentSlide(3)"></span>
 </div>
+<?php
+$queryBuildResult = "";
+if ($SearchString != "") {
+    for ($i = 0; $i < count($searchValues); $i++) {
+        if ($i != 0) {
+            $queryBuildResult .= "AND ";
+        }
+        $queryBuildResult .= "SI.SearchDetails LIKE '%$searchValues[$i]%' ";
+    }
+    if ($queryBuildResult != "") {
+        $queryBuildResult .= " OR ";
+    }
+    if ($SearchString != "" || $SearchString != null) {
+        $queryBuildResult .= "SI.StockItemID ='$SearchString'";
+    }
+}
+
+$Offset = $PageNumber * $ProductsOnPage;
+
+$ShowStockLevel = 1000;
+if ($CategoryID == "") {
+    if ($queryBuildResult != "") {
+        $queryBuildResult = "WHERE " . $queryBuildResult;
+    }
+
+    $Query = "
+                SELECT SI.StockItemID, SI.StockItemName, SI.MarketingComments, ROUND(TaxRate * RecommendedRetailPrice / 100 + RecommendedRetailPrice,2) as SellPrice,
+                (CASE WHEN (SIH.QuantityOnHand) >= ? THEN 'Ruime voorraad beschikbaar.' ELSE CONCAT('Voorraad: ',QuantityOnHand) END) AS QuantityOnHand, 
+                (SELECT ImagePath
+                FROM stockitemimages 
+                WHERE StockItemID = SI.StockItemID LIMIT 1) as ImagePath,
+                (SELECT ImagePath FROM stockgroups JOIN stockitemstockgroups USING(StockGroupID) WHERE StockItemID = SI.StockItemID LIMIT 1) as BackupImagePath
+                FROM stockitems SI
+                JOIN stockitemholdings SIH USING(stockitemid)
+                " . $queryBuildResult . "
+                GROUP BY StockItemID
+                ORDER BY " . $Sort . " 
+                LIMIT ?  OFFSET ?";
+
+
+    $Statement = mysqli_prepare($Connection, $Query);
+    mysqli_stmt_bind_param($Statement, "iii", $ShowStockLevel, $ProductsOnPage, $Offset);
+    mysqli_stmt_execute($Statement);
+    $ReturnableResult = mysqli_stmt_get_result($Statement);
+    $ReturnableResult = mysqli_fetch_all($ReturnableResult, MYSQLI_ASSOC);
+
+    $Query = "
+            SELECT count(*)
+            FROM stockitems SI
+            $queryBuildResult";
+    $Statement = mysqli_prepare($Connection, $Query);
+    mysqli_stmt_execute($Statement);
+    $Result = mysqli_stmt_get_result($Statement);
+    $Result = mysqli_fetch_all($Result, MYSQLI_ASSOC);
+} else {
+
+    if ($queryBuildResult != "") {
+        $queryBuildResult .= " AND ";
+    }
+
+    $Query = "
+                SELECT SI.StockItemID, SI.StockItemName, SI.MarketingComments, 
+                ROUND(SI.TaxRate * SI.RecommendedRetailPrice / 100 + SI.RecommendedRetailPrice,2) as SellPrice, 
+                (CASE WHEN (SIH.QuantityOnHand) >= ? THEN 'Ruime voorraad beschikbaar.' ELSE CONCAT('Voorraad: ',QuantityOnHand) END) AS QuantityOnHand,
+                (SELECT ImagePath FROM stockitemimages WHERE StockItemID = SI.StockItemID LIMIT 1) as ImagePath,
+                (SELECT ImagePath FROM stockgroups JOIN stockitemstockgroups USING(StockGroupID) WHERE StockItemID = SI.StockItemID LIMIT 1) as BackupImagePath           
+                FROM stockitems SI 
+                JOIN stockitemholdings SIH USING(stockitemid)
+                JOIN stockitemstockgroups USING(StockItemID)
+                JOIN stockgroups ON stockitemstockgroups.StockGroupID = stockgroups.StockGroupID
+                WHERE " . $queryBuildResult . " ? IN (SELECT StockGroupID from stockitemstockgroups WHERE StockItemID = SI.StockItemID)
+                GROUP BY StockItemID
+                ORDER BY " . $Sort . " 
+                LIMIT ? OFFSET ?";
+
+    $Statement = mysqli_prepare($Connection, $Query);
+    mysqli_stmt_bind_param($Statement, "iiii", $ShowStockLevel, $CategoryID, $ProductsOnPage, $Offset);
+    mysqli_stmt_execute($Statement);
+    $ReturnableResult = mysqli_stmt_get_result($Statement);
+    $ReturnableResult = mysqli_fetch_all($ReturnableResult, MYSQLI_ASSOC);
+
+    $Query = "
+                SELECT count(*)
+                FROM stockitems SI 
+                WHERE " . $queryBuildResult . " ? IN (SELECT SS.StockGroupID from stockitemstockgroups SS WHERE SS.StockItemID = SI.StockItemID)";
+    $Statement = mysqli_prepare($Connection, $Query);
+    mysqli_stmt_bind_param($Statement, "i", $CategoryID);
+    mysqli_stmt_execute($Statement);
+    $Result = mysqli_stmt_get_result($Statement);
+    $Result = mysqli_fetch_all($Result, MYSQLI_ASSOC);
+}
+?>
+
+
 
 
     <div>
