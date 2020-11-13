1a8e351d2350d39ae816b6222896dae41ac40d19
Rembrandt-T <s1153038@student.windesheim.nl>
2020-11-13 11:17:28 +0100

    Contactpagina toegevoegd

---
+++ contact.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Contact</title>
+    <link rel='stylesheet' href='style.css'>
+</head>
+
+<?php
+include __DIR__ . "/Header.php";
+?>
+
+<h1>Contact</h1>
+Telefoonnummer: 06123456<br>
+E-mail: nerdygadgets@gmail.com
+<h2>Sociale media</h2>
+Twitter: <a href='twitter.com'>Nerdygadgets</a><br>
+Facebook: <a href='facebook.com'>Nerdygadgets</a>
