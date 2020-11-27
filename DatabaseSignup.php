8afd88a9fdae69466a79a52f6d9c7e3cab4604e0
iRudyzz <rudy01@live.nl>
2020-11-27 11:51:59 +0100

    ja mooi

--- DatabaseSignup.php
+++ DatabaseSignup.php
@@ -1,4 +1,5 @@
 <?php
+include "connect.php";
 
 $gebruikersNaam = "";
 $gebruikersWachtwoord = "";
@@ -9,44 +10,34 @@ $gebruikersZipcode = "";
 $gebruikersPhone = "";
 $gebruikersEmail = "";
 
-
-
-$gebruikersNaam = $_POST["Username"];
-$gebruikersWachtwoord = $_POST["Password"];
+$gebruikersNaam = mysqli_real_escape_string($Connection, $_POST["Username"]);
+$gebruikersWachtwoord = mysqli_real_escape_string($Connection, $_POST["Password"]);
+$gebruikersFirstNaam = mysqli_real_escape_string($Connection, $_POST["firstname"]);
+$gebruikersLastNaam = mysqli_real_escape_string($Connection, $_POST["lastname"]);
+$gebruikersAddress = mysqli_real_escape_string($Connection, $_POST["address"]);
+$gebruikersZipcode = mysqli_real_escape_string($Connection, $_POST["zipcode"]);
+$gebruikersPhone = mysqli_real_escape_string($Connection, $_POST["phonenumber"]);
+$gebruikersEmail = mysqli_real_escape_string($Connection, $_POST["email"]);
 
 if (isset($_POST["phonenumber"])){
-    $Query = "INSERT INTO webcustomers ('firstname','lastname','username','password','address','zipcode','phonenumber','emailaddress')
-    VALUES (?,?,?,?,?,?,?,?)"
+        $Query ="INSERT INTO webcustomer (Firstname,Lastname,Username,Password,Address,Zipcode,Phonenumber,Email)
+        VALUES ('$gebruikersFirstNaam', '$gebruikersLastNaam', '$gebruikersNaam', '$gebruikersWachtwoord','$gebruikersAddress','$gebruikersZipcode','$gebruikersPhone','$gebruikersEmail');";
+        $Statement = mysqli_prepare($Connection, $Query);
+        mysqli_stmt_bind_param($Statement, "isisisis", $gebruikersFirstNaam, $gebruikersLastNaam, $gebruikersNaam, $gebruikersWachtwoord, $gebruikersAddress, $gebruikersZipcode, $gebruikersPhone, $gebruikersEmail);
 
-    $Statement = mysqli_prepare($Connection, $Query);
-    mysqli_stmt_bind_param($Statement, "isisisis", $gebruikersFirstNaam, $gebruikersLastNaam, $gebruikersNaam, $gebruikersWachtwoord, $gebruikersAddress, $gebruikersZipcode, $gebruikersPhone, $gebruikersEmail);
-
-
-}
-else {
-    $Query = "INSERT INTO webcustomers ('firstname','lastname','username','password','address','zipcode','emailaddress')
-    VALUES (?,?,?,?,?,?,?)"
+        mysqli_stmt_execute($Statement);
+    } else {
+    $Query = "INSERT INTO webcustomer (firstname,lastname,username,password,address,zipcode,email)
+    VALUES (?,?,?,?,?,?,?);";
 
     $Statement = mysqli_prepare($Connection, $Query);
     mysqli_stmt_bind_param($Statement, "isisisis", $gebruikersFirstNaam, $gebruikersLastNaam, $gebruikersNaam, $gebruikersWachtwoord, $gebruikersAddress, $gebruikersZipcode, $gebruikersEmail); 
 }
 
-                
+$Queryun= mysqli_query($Connection, "SELECT Username FROM webcostumer WHERE Username LIKE" . mysqli_real_escape_string($Connection, $_POST['Username']) . ";");
 
-
-    $Statement = mysqli_prepare($Connection, $Query);
-    mysqli_stmt_bind_param($Statement, "ii", $gebruikersNaam, $gebruikersWachtwoord);
-    mysqli_stmt_execute($Statement);
-    $ReturnableResult = mysqli_stmt_get_result($Statement);
-    $ReturnableResult = mysqli_fetch_all($ReturnableResult, MYSQLI_ASSOC);
-
-
-if ($ReturnableResult["username"] == $gebruikersNaam && $ReturnableResult["password"] == $gebruikersWachtwoord ){
-    print("<h1> Welkom $ReturnableResult['firstname'] $ReturnableResult['lastname'], je bent zojuist ingelogd!</h1>");
-    $_SESSION["loggedin"] == true;
-}
-else {
+if($_POST["Username"] == $Queryun){
+    print("Huge Succ");
+} else {
     print("<h1>Gegevens incorrect, probeer opnieuw</h1>");
 }
-
-
